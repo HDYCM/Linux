20e6c65f5b31a7af348abbd7acca5104e0eea43d
aadorable <1615691183@qq.com>
2018-04-22 23:25:01 +0800

    zombie

---
+++ zombie/zombie.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+    int ret = fork();
+    if(ret < 0){
+        perror("fork");
+        return 1;
+    }
+    else if(ret > 0){
+        printf("parent[%d] is sleeping...\n", getpid());
+        sleep(30);
+    }
+    else{
+        printf("child[%d] is begin z...\n",getpid());
+        sleep(5);
+        exit(EXIT_SUCCESS);
+    }
+    return 0;
+}
