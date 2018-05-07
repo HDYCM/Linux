c28ed7c8b4a49f2c631fe5ea069885268fa26a1a
aadorable <1615691183@qq.com>
2018-05-07 16:51:42 +0800

    waitpid

---
+++ waitpid/waitpid.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(){
+    printf("father %d \n", getpid());
+    int ret = fork();
+    if(ret == 0){
+        printf("child1 %d\n", getpid());
+        sleep(3);
+        exit(0);
+    }
+    ret = fork();
+    if(ret == 0){
+        printf("child2 %d\n", getpid());
+        sleep(5);
+        exit(0);
+    }
+    while(1){
+        ret = waitpid(-1, NULL, WNOHANG);
+        printf("ret = %d\n", ret);
+        if(ret > 0){
+            printf("wait child %d\n\n", ret);
+        }
+        else if(ret < 0){
+            break;
+        }
+        else{
+            printf("father do work!\n\n");
+        }
+        sleep(1);
+    }
+    return 0;
+}
