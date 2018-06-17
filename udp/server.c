e56509c7276ff7b10adc4a6c29e244343e52301d
aadorable <1615691183@qq.com>
2018-06-17 23:54:40 +0800

    udp

---
+++ udp/server.c
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////
+//服务器逻辑：
+//1.启动（初始化）
+//2.进入一个死循环（事件循环）
+// a）从 socket 中读取请求（Request）
+// b）根据 Request 的内容计算生成 Response
+// c）把 Response 响应写回到socket
+//
+// 此处为了实现方便，我们实现一个 echo_server（回显服务器）
+///////////////////////////////////////////////////////////////
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+typedef struct sockaddr sockaddr;
+typedef struct sockaddr_in sockaddr_in;
+
+// ./server 127.0.0.1 9090
+int main(int argc, char* argv[]){
+    //0.校验命令行参数
+    if(argc != 3){
+        printf("Usage ./server [ip] [port]\n");
+        return 1;
+    }
+    //1.服务器的初始化
+    // a）创建 socket
+    // AF_INET:ipv4协议
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(fd < 0){
+        perror("socket");
+        return 1;
+    }
+    //b）绑定ip地址端口号
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    //点分十进制的字符串ip地址转换成数字（网络字节序）
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htos(atoi(argv[2]));
+    int ret = bind(fd, (sockaddr*)&addr, sizeof(addr));
+    if(ret < 0){
+        perror("bind");
+        return 1;
+    }
+    return 0;
+}
+
